0595a829a6691a13ff3c3f9e5a4b32bf32f4f5b1
kawint <47197509+kawint@users.noreply.github.com>
2022-05-03 14:45:30 -0400

    ..

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Color } from 'three';
 import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls.js';
 import { fpsCamera } from './components/camera';
 import { Vector3, Vector2, Raycaster } from 'three';
-import { Mouse } from './components/mouse';
+// import { Mouse } from './components/mouse';
 
 const NUM_SPLIT = 4;
 var TOTAL = 1;
@@ -467,7 +467,7 @@ let html = "<link rel='preconnect' href='ht tps://fonts.gstatic.com'> \
   <div style='text-align: center;margin-top:100px;'><span style='font-size:6em; font-weight: bold; font-family: courier, sans-serif;'><img src='./src/aim.ico'width=70; height=70;> Aim Lab</span></div> \
   <div style='text-align: center;'><span style='font-size:3em; font-family: courier, sans-serif;'>Practice your aim!</span></div> \
   <div><br/></div>\
-  <div id='ball' style='background:clear; position:absolute;'><img src='ball.png'; width=164; height=137;></div> \
+  <div id='ball' style='background:clear; position:absolute;'><img src='src/ball.png'; width=164; height=137;></div> \
   <div><br/></div>\
   <div style='text-align: center;'><span style='font-size:2em; font-weight: 300; font-family: courier, sans-serif; font-weight: bold;'>INSTRUCTIONS:</span></div> \
   <div style='text-align: center;'><span style='font-size:2em; font-weight: 300; font-family: courier, sans-serif; '>Move: WASD, Look: MOUSE</span></div> \
